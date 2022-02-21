1fe3b895818d4a501c730d55ba33104ecc8d0e0d
vampire <975826228@qq.com>
2022-02-21 21:44:48 +0800

    test

---
+++ src/test/com/xskq/test/test.java
@@ -0,0 +1,36 @@
+package xskq.test;
+
+import com.xskq.dao.AdminMapper;
+import com.xskq.model.Admin;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+/**
+ * Title: <br>
+ * Description: <br>
+ * Company: wondersgroup.com <br>
+ *
+ * @author zwq
+ * @version 1.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+public class test {
+
+    @Autowired
+    private AdminMapper adminMapper;
+
+    @Test
+    public void test() {
+        System.out.println("进来了");
+        List<Admin> admins = adminMapper.getAdmin();
+        for (Admin admin : admins) {
+            System.out.println(admin.getUsername());
+        }
+    }
+}
