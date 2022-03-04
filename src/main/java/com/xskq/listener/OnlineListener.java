eca52ebeaeaf03ead196416cd41ad4417c09fc6b
vampire <975826228@qq.com>
2022-03-04 14:32:27 +0800

    listener

--- src/main/java/com/xskq/listener/OnlineListener.java
+++ src/main/java/com/xskq/listener/OnlineListener.java
@@ -23,58 +23,30 @@ public class OnlineListener implements HttpSessionAttributeListener, HttpSession
     public void attributeAdded(HttpSessionBindingEvent httpSessionBindingEvent) {
         ServletContext servletContext = httpSessionBindingEvent.getSession().getServletContext();
         String name = httpSessionBindingEvent.getName();
+        Object value = httpSessionBindingEvent.getValue();
         if (name.equals("code")) {
-            int number = (int) servletContext.getAttribute("number");
-            List<Object> allUser = (List<Object>) servletContext.getAttribute("allUser");
-            number++;
-            //allUser.add()
-            servletContext.setAttribute("number", number);
-            servletContext.setAttribute("allUser", allUser);
+            HashSet<Object> hashSet;
+            if (servletContext.getAttribute("online") == null) {
+                hashSet = new HashSet<>();
+            } else {
+                hashSet = (HashSet<Object>) servletContext.getAttribute("online");
+            }
+            hashSet.add(httpSessionBindingEvent.getValue());
+            servletContext.setAttribute("number", hashSet.size());
+            servletContext.setAttribute("online", hashSet);
         }
-
-        /*String name = httpSessionBindingEvent.getName();
-        System.out.println("add监听器：" + name);
-        if (name.equals("user")) {
-            list.add(httpSessionBindingEvent.getValue());
-        }
-        //list去重复
-        HashSet<Object> hs = new HashSet<>(list);
-        ServletContext application = httpSessionBindingEvent.getSession().getServletContext();
-        application.setAttribute("number", hs.size());
-        application.setAttribute("alluser", hs);*/
-        //getName:拿到设置的名字
-        //getValue:拿到值
-		/*String name = httpSessionBindingEvent.getName();
-		HttpSession session = se.getSession();
-		System.out.println("name:"+name);
-		System.out.println(httpSessionBindingEvent.getValue());
-		System.out.println(httpSessionBindingEvent.getSession());
-		list.add(session);
-		//list去重复
-		HashSet<Object> hs = new HashSet<Object>(list);
-		ServletContext application = httpSessionBindingEvent.getSession().getServletContext();
-		application.setAttribute("number", hs.size());*/
     }
 
     @Override
     public void attributeRemoved(HttpSessionBindingEvent httpSessionBindingEvent) {
         ServletContext application = httpSessionBindingEvent.getSession().getServletContext();
-        List<Object> list = (List<Object>) application.getAttribute("alluser");
+        HashSet<Object> hashSet = (HashSet<Object>) application.getAttribute("online");
         String name = httpSessionBindingEvent.getName();
-        System.out.println("remove监听器：" + name);
-        if (name.equals("user")) {
-            list.remove(httpSessionBindingEvent.getValue());
+        if (name.equals("code")) {
+            hashSet.remove(httpSessionBindingEvent.getValue());
         }
-        application.setAttribute("number", list.size());
-        application.setAttribute("alluser", list);
-		/*System.out.println("attributeRemoved");
-		String name = httpSessionBindingEvent.getName();
-		System.out.println("name:"+name);
-		HttpSession session = httpSessionBindingEvent.getSession();
-		list.remove(session);
-		HashSet<Object> hs = new HashSet<Object>(list);
-		ServletContext application = httpSessionBindingEvent.getSession().getServletContext();
-		application.setAttribute("number", hs.size());*/
+        application.setAttribute("number", hashSet.size());
+        application.setAttribute("online", hashSet);
     }
 
     @Override
